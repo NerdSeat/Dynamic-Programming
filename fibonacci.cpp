47537772303da0558f978ed97e91aa88ff84135b
NerdSeat <josephblister@gamil.com>
2020-12-25 02:11:48 +0200

    Added memoization to the fibonacci function

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,21 +5,26 @@ numbers are both on.
 
 n:      1, 2, 3, 4, 5, 6, 7, 8...
 fib(n): 1, 1, 2, 3, 5, 8, 13,21...
-
 ***************************************************************************/
 
 #include<iostream>
+#include<unordered_map>
+typedef unsigned long long ull;
 
-
-long fibo(int n)
+ull fibo(int n,std::unordered_map<int,ull>& memo)
 {
+    auto val = memo.find(n);
     if(n<=2)
         return 1;
-    return fibo(n-1) + fibo(n-2);
+    if(val!=memo.end())
+        return memo[n];
+    memo[n] = fibo(n-1,memo) + fibo(n-2,memo);
+    return memo[n];
 }
 
 int main()
 {
-    int n = 9;
-    std::cout<<fibo(n)<<std::endl;
+    std::unordered_map<int, ull> memo;
+    int n = 50;
+    std::cout<<fibo(n,memo)<<std::endl;
 }
